823557446645b2c1bb0e4ae3fece3bd0a843a182
kevin.ramosb <kevin.ramosb@salud.gob.sv>
2023-01-09 15:42:32 -0600

    modificar estructura con archivos dedicados para cada grafica

--- js/chartInfraccionesPenales.js
+++ js/chartInfraccionesPenales.js
@@ -1,49 +1,49 @@
 // Seleccionar elemento chart
-const chartPieChart = d3.select("#pieChart");
+const chart = d3.select("#pieChart");
 const options = d3.select("#options");
 
 //generar margenes automaticos
-const marginsChart4 = {
+const margins = {
   top: 50,
   right: 20,
   bottom: 100,
   left: 150,
   end: 20,
 };
-// const totaWithChart4 = +chart.style("width").slice(0, -2);
-const totaWithChart4 = 650;
-const totalHeigthChart4 = (totaWithChart4 * 9) / (totaWithChart4 / 90);
-const chartWithPieChart =
-  totaWithChart4 - marginsChart4.left - marginsChart4.right;
-const chartHeigthPieChart = totalHeigthChart4 - marginsChart4.top - marginsChart4.bottom - marginsChart4.end;
+// const totalWith = +chart.style("width").slice(0, -2);
+const totalWith = 650;
+const totalHeigthChart = (totalWith * 9) / (totalWith / 90);
+const chartWith = totalWith - margins.left - margins.right;
+const chartHeigth =
+  totalHeigthChart - margins.top - margins.bottom - margins.end;
 
 // crear dimensiones del chart
-const svgPieChart = chartPieChart
+const svgPieChart = chart
   .append("svg")
-  .attr("width", totaWithChart4)
-  .attr("height", totalHeigthChart4)
-  .attr("class", "grafPieChart")
+  .attr("width", totalWith)
+  .attr("height", totalHeigthChart)
+  .attr("class", "graf")
   .append("g")
-  .attr("transform", `translate(${marginsChart4.left}, ${marginsChart4.top})`);
+  .attr("transform", `translate(${margins.left}, ${margins.top})`);
 
 // generar escaladores para los ejes
-const x = d3.scaleBand().range([0, chartWithPieChart]);
+const x = d3.scaleBand().range([0, chartWith]);
 // invertir y (por defecto está inverso)
-const y = d3.scaleLinear().range([chartHeigthPieChart, 0]);
+const y = d3.scaleLinear().range([chartHeigth, 0]);
 
 // // crear ejes del chart
-const xAxisPieChart = svgPieChart
+const xAxis = svgPieChart
   .append("g")
-  .attr("transform", "translate(0," + chartHeigthPieChart + ")")
+  .attr("transform", "translate(0," + chartHeigth + ")")
   .attr("class", "axisX");
 
-const yAxisPieChart = svgPieChart.append("g").attr("class", "axisY");
+const yAxis = svgPieChart.append("g").attr("class", "axisY");
 
 // por defecto filtrar los datos de este parametro
 let parameter = "Agresión sexual con penetración";
 
 // funcion que extrae los datos del dataset
-const readDataChartPieChart = async () => {
+const readDatachart = async () => {
   const {
     Respuesta: {
       Datos: { Metricas: metrics },
@@ -58,8 +58,8 @@ const readDataChartPieChart = async () => {
 };
 
 // cargar los datos y crear los dominios de las ejes
-const loadDataPieChart = async () => {
-  const data = await readDataChartPieChart(); // obtener los datos
+const loadData = async () => {
+  const data = await readDatachart(); // obtener los datos
 
   // filtrar unicamente los datos del parametro indiciado(filtro)
   const newData = data.filter((d) => d.Parametro === parameter);
@@ -70,12 +70,12 @@ const loadDataPieChart = async () => {
   y.domain([0, d3.max(newData, (d) => d.Valor) * 1.1]);
 
   // efectos al actualizar el filtro
-  xAxisPieChart.transition().duration(1000).call(d3.axisBottom(x));
-  yAxisPieChart.transition().duration(1000).call(d3.axisLeft(y));
+  xAxis.transition().duration(1000).call(d3.axisBottom(x));
+  yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
   svgPieChart
     .append("text")
-    .attr("x", chartWithPieChart / 2)
+    .attr("x", chartWith / 2)
     .attr("y", -10)
     .attr("class", "labels")
     .attr("text-anchor", "middle")
@@ -83,20 +83,20 @@ const loadDataPieChart = async () => {
 
   svgPieChart
     .append("g")
-    .attr("transform", `translate(-80, ${chartHeigthPieChart / 2})`)
+    .attr("transform", `translate(-80, ${chartHeigth / 2})`)
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("text-anchor", "middle")
     .attr("class", "labels")
     .text("Unidades");
 
-  renderPieChart(newData);
+  render(newData);
 };
 
 // funcion que genera y actualiza la grafica según los datos obtenidos
-const renderPieChart = (data) => {
+const render = (data) => {
   // mostrar detalles del dato
-  const tooltip = chartPieChart
+  const tooltip = chart
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip");
@@ -112,7 +112,7 @@ const renderPieChart = (data) => {
     .attr("x", (d) => x(d.Agno))
     .attr("y", (d) => y(d.Valor))
     .attr("width", x.bandwidth())
-    .attr("height", (d) => chartHeigthPieChart - y(d.Valor))
+    .attr("height", (d) => chartHeigth - y(d.Valor))
     .attr("fill", "#ff000099");
 
   // ejecutar eventos la pasar el mouse por el elemento
@@ -138,7 +138,7 @@ const renderPieChart = (data) => {
 
 // agrega los parametros obtenidos del dataset a elemento de seleccion
 const optionsFilter = async () => {
-  const data = await readDataChartPieChart();
+  const data = await readDatachart();
 
   const filters = new Set(data.map((d) => d.Parametro)); // solo valores unicos
 
@@ -151,8 +151,8 @@ const optionsFilter = async () => {
 // aplica el filtro seleccionado para actualizar los datos
 const optionChanged = (option) => {
   parameter = option;
-  loadDataPieChart();
+  loadData();
 };
 
 optionsFilter();
-loadDataPieChart();
+loadData();
